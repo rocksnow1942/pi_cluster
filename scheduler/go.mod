170eba790a9a46e3e099560cb2ca749980273a8a
rocksnow1942 <rocksnow1942@gmail.com>
2024-09-19 20:01:46 -0700

    demo

---
+++ scheduler/go.mod
@@ -0,0 +1,3 @@
+module github.com/rocksnow1942/pi_cluster/scheduler
+
+go 1.22.1
---
+++ scripts/upload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -eEuo pipefail
+
+# This script is used to upload the built artifacts to the Pi.
+
+# Usage: ./scripts/upload.sh <path-to-artifact> <path-on-pi>
