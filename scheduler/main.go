4eb74849d1b5583fa6928b851898e012a15f9c30
rocksnow1942 <rocksnow1942@gmail.com>
2024-09-26 01:28:52 -0700

    update parts

--- scheduler/main.go
+++ scheduler/main.go
@@ -9,7 +9,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Received request from", r.RemoteAddr)
 		// echo back the reqeust url
-		fmt.Fprint(w, "Hello, World!\n"+r.URL.EscapedFragment())
+		fmt.Fprint(w, "Hello, World!\nFrom:"+r.URL.Path)
 	})
 	fmt.Println("Started server on port 80!!")
 	http.ListenAndServe(":80", nil)
